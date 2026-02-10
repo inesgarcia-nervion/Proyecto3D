78a6d48629ab62a8a2d38746ad5d27032b249f14
inesgarcia-nervion <ines.garcia@iesnervion.es>
2026-02-10 22:36:45 +0100

    Falta colisión de cámara con paredes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -73,7 +73,7 @@ public class PlayerController : MonoBehaviour
         // Correr
         bool running = Keyboard.current.leftShiftKey.isPressed;
         animator.SetBool("isRunning", running);
-        speed = running ? 20f : 12f;
+        speed = running ? 7f : 4f;
 
         // Registrar cuando se presiona el salto
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
